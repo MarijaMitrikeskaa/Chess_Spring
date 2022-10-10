c91b076acf35cb0684b1de087dffac2e1f68a238
Krste Ristevski <Krste.Ristevski@netcetera.com>
2022-10-10 16:54:12 +0200

    port 55505

---
+++ src/main/java/com/fict/workinggroups/chess_puzzles/FooController.java
@@ -0,0 +1,13 @@
+package com.fict.workinggroups.chess_puzzles;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FooController {
+
+    @RequestMapping("/foo")
+    public String fooMethod() {
+        return "foo";
+    }
+}
