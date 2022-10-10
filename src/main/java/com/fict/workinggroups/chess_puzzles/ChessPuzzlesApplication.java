b0797a0cf84801c9e368790e9fe3e397b3d3c3b4
Krste Ristevski <Krste.Ristevski@netcetera.com>
2022-10-10 16:33:22 +0200

    init zip from initializer

---
+++ src/main/java/com/fict/workinggroups/chess_puzzles/ChessPuzzlesApplication.java
@@ -0,0 +1,13 @@
+package com.fict.workinggroups.chess_puzzles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessPuzzlesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessPuzzlesApplication.class, args);
+	}
+
+}
