1c5dded57d43bbe8f889071dad0caa160b69ed44
Viktorija Nikolovska <viktorija.nikolovska@hotmail.com>
2023-02-02 23:35:22 +0100

    code reformat

--- src/main/java/com/fict/workinggroups/chess_puzzles/web/rest/PlayedFenRestController.java
+++ src/main/java/com/fict/workinggroups/chess_puzzles/web/rest/PlayedFenRestController.java
@@ -6,7 +6,10 @@ import com.fict.workinggroups.chess_puzzles.model.dto.PlayedFenDto;
 import com.fict.workinggroups.chess_puzzles.service.PlayedFensService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/playedFen")
@@ -15,7 +18,7 @@ public class PlayedFenRestController {
     @Autowired
     private PlayedFensService playedFensService;
 
-    @PostMapping(value = "/makeAMove" )
+    @PostMapping(value = "/makeAMove")
     public ResponseEntity makeAMove(@ModelAttribute PlayedFenDto playedFenDto) {
         try {
             playedFensService.updateLeaderboard(playedFenDto);
